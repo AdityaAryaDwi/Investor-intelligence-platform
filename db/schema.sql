fddf960326ef6319825cd33c5f2d521762e723d5
Aditya Arya <aditya8ind8@gmail.com>
2025-06-29 07:13:43 +0530

    Initial structure and database schema

--- db/schema.sql
+++ db/schema.sql
@@ -9,3 +9,15 @@ create table if not exists stock_prices(
     volume bigint,
     unique (symbol,trading_day)
 );
+
+create table if not exists portfolio_positions(
+    id serial primary key,
+    symbol varchar(10) not null,
+    buy_date date not null,
+    quantity integer not null,
+    buy_price numeric not null,
+    current_price numeric,
+    sector varchar(50),
+    notes text
+);
+
